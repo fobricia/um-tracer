ca4f747cc23d1bba666e30165ff4abbadca88db0
_xeroxz <xerox@back.engineer>
2021-05-19 16:33:30 -0700

    added ref builds and dependencies

---
+++ src/vmptest/main.cpp
@@ -0,0 +1,17 @@
+#include <intrin.h>
+#include <iostream>
+#include <Windows.h>
+#include "ia32.h"
+
+int main()
+{
+	/*cpuid_eax_01 cpuid_info;
+	__cpuid((int*)&cpuid_info, 1);
+
+	if (cpuid_info.cpuid_feature_information_ecx.avx_support)
+		return 1;
+
+	return 0;*/
+
+	MessageBoxA(NULL, "Hello World", "Hello World", NULL);
+}
